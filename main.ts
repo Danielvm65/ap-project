ab8eb87d16d8e17025f1b70ef58f159b1fb31f66
Danielvm65 <156251834+Danielvm65@users.noreply.github.com>
2024-02-28 21:04:47 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,38 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.sayText("Holding ")
+    lastTimestamp = game.runtime()
+})
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
+    Powershot(game.runtime() - lastTimestamp)
+    mySprite.sayText(mySprite2)
+})
+function Powershot (Time: number) {
+    mySprite2 = Time * 1.2
+}
+let mySprite2 = 0
+let lastTimestamp = 0
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . . . . 3 3 . . . . . . . 
+    3 . . . . . 3 3 3 3 . . . . . 3 
+    3 . . . . . 3 3 3 3 . . . . . 3 
+    3 . . . . . 3 3 3 3 . . . . . 3 
+    3 3 . . . . 3 3 3 3 . . . . 3 3 
+    3 3 . . . . 3 3 3 3 . . . . 3 3 
+    3 3 . . . . 3 3 3 3 . . . . 3 3 
+    3 3 . . . . 3 3 3 3 . . . . 3 3 
+    3 3 . . . 3 3 3 3 3 3 . . . 3 3 
+    3 3 . . . 3 3 5 5 3 3 . . . 3 3 
+    3 3 3 . . 3 2 5 5 2 3 . . 3 3 3 
+    3 3 3 3 3 3 2 5 5 2 3 3 3 3 3 3 
+    . 3 3 3 3 3 2 5 5 2 3 3 3 3 3 . 
+    . . 3 3 3 3 2 f f 2 3 3 3 3 . . 
+    . . . . . . 4 4 4 4 . . . . . . 
+    . . . . . . . 4 4 . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 50, 50)
+mySprite.setStayInScreen(false)
+lastTimestamp = game.runtime()
+tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 0))
+tiles.setCurrentTilemap(tilemap`level1`)
+scene.cameraFollowSprite(mySprite)
